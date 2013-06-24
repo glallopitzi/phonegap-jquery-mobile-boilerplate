db1aa09aaf64b72743e2a587e3b9ac463d52cd6b
glallopitzi <giancarlolallopizzi@gmail.com>
2013-06-24 22:16:19 +0200

    make a call

--- assets/www/index.js
+++ assets/www/index.js
@@ -1,36 +1,118 @@
+var DEBUG = false;
+var dataAux;
+
 var app = {
 		
+		debug : DEBUG,
 		
-		baseWebsiteRestUrl : 'http://it.jobrapido.com/fo/api/v1/search/',
+		theWhat : '',
+		theWhere : '',
+		theRadius : '_empty',
+		theCountry : 'it',
+		theProtocol : 'http://',
+		theDomain : '.jobrapido.com/',
+		theDo : 'api.feed',
+		theLogin : 'Sn0Wb411',
+		thePassword : 'suspended25102012',
+		theFormat : 'json',
 		
 		initialize : function(){
-
-			console.log("begin");
-		  
-			var xhr = new XMLHttpRequest();
-			 xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-			  // Response handlers.
-			  xhr.onload = function () {
-			     var repos = JSON.parse(xhr.response), i, reposHTML = "";
-			     for (i = 0; i < repos.repositories.length; i++) {
-			       reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-			     }
-			     document.getElementById("allRepos").innerHTML = reposHTML;
-			  };
-
-			  xhr.onerror = function () {
-			     alert('error making the request.');
-			  };
-
-			xhr.send();
+			app.log( "initialize" );
+			
+		  	app.theWhat = 'java';
+		  	app.theWhere = 'milano',
+			app.makeSearch();
+		},
+		
+		log : function ( msg ){
+			if ( this.debug ){
+				console.log( msg );
+			}
+		},
+		
+		buildRequestUrl : function(){
+			app.log( "buildRequestUrl" );
+			
+			var _result = "";
+			_result = _result + this.theProtocol + this.theCountry + this.theDomain;
+			_result = _result + '?do=' + this.theDo + '&login=' + this.theLogin + '&password=' + this.thePassword + '&format=' + this.theFormat;
+			_result = _result + '&w=' + this.theWhat + '&l=' + this.theWhere;
+			
+			app.log( "buildRequestUrl, _result: " + _result );
+			return _result;
+		},
+		
+		showResult : function ( data ){
+			var advertsContainer = $("#allRepos");
+			var advertsList = $("<ul data-role=\"listview\"></ul>");
+			
+			$.each( data.searchResults.results, function(i, item){
+				advertsList.append("<li><a href=\"" + item.url + "\">" + item.title + "</a></li>");
+			});
+			
+			advertsContainer.append(advertsList);
+			$.mobile.navigate("#result");
+		},
+		
+		makeSearch : function(){
+			app.log( "makeSearch" );
+			
+			var _url = this.buildRequestUrl();
+			$.ajax({
+				url : _url,
+				dataType: 'jsonp',
+			}).done( function( data ) {
+				console.log('data: ' + data, data);
+				dataAux = data;
+				app.showResult(data);
+			});
 		}
+
 };
 
-$(document).ready(function(){
+
+
+if (! DEBUG){
+	//*********************************************************
+	// Wait for Cordova to Load
+	//*********************************************************
+	var deviceReadyDeferred = $.Deferred();
+	var jqmReadyDeferred = $.Deferred();
+
+	document.addEventListener("deviceReady", deviceReady, false);
+
+	function deviceReady() {
+	  deviceReadyDeferred.resolve();
+	}
+
+	$(document).on("mobileinit", function () {
+	  jqmReadyDeferred.resolve();
+	});
+
+	$.when(deviceReadyDeferred, jqmReadyDeferred).then(doWhenBothFrameworksLoaded);
+
+	function doWhenBothFrameworksLoaded() {
+	  app.initialize();
+	}
+
+
+
+} else {
+	console.log('DEBUG enabled');
+	app.initialize();
+}
+
+
+$(document).ready( function() {
+	console.log("document ready");
 	
-	$('#searchFormSubmit').click(function(){
-		console.log("submitted");
-		$('#searchForm').submit();
+	$('#searchFormSubmit').click( function( e ) {
+		
+		app.theWhat = $('#what').val() !== '' ? $('#what').val() : '_empty';
+		app.theWhere = $('#where').val() !== '' ? $('#where').val() : '_empty';
+		app.makeSearch();
+
+		//e.preventDefault();
 	});
 	
 });
